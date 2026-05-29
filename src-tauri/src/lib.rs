a1270661a63f2649f8914421de39b3f9006223fa
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2026-05-29 16:20:28 +0200

    load csv, generate, save

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,161 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs::{self, File};
+use std::path::PathBuf;
+use std::thread;
+
+use tauri::{AppHandle, Manager, State};
+use tokio::sync::{mpsc, oneshot};
+
+use ab_glyph::{FontArc, PxScale};
+use image::Rgba;
+use imageproc::drawing::draw_text_mut;
+
+#[derive(Debug)]
+enum Event {
+    Csv(PathBuf, oneshot::Sender<Result<Vec<String>, String>>),
+    Generate(
+        PathBuf,
+        Vec<usize>,
+        oneshot::Sender<Result<PathBuf, String>>,
+    ),
+    Save(PathBuf, oneshot::Sender<Result<PathBuf, String>>),
+}
+
+fn worker(
+    handle: AppHandle,
+    mut rx: mpsc::Receiver<Event>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut csv_path: Option<PathBuf> = None;
+    let mut csv_rdr: Option<csv::Reader<File>> = None;
+
+    let data_dir = handle.path().app_data_dir().unwrap();
+    let outputs_dir = data_dir.join("outputs");
+    let output_file = outputs_dir.join("test.png");
+
+    while let Some(event) = rx.blocking_recv() {
+        match event {
+            Event::Csv(path, resp) => {
+                let f = File::open(&path)?;
+                let mut rdr = csv::Reader::from_reader(f);
+                let headers: Vec<String> = match rdr.headers() {
+                    Ok(record) => record.iter().map(|h| h.to_string()).collect(),
+                    Err(e) => {
+                        eprintln!("csv error: {e}");
+                        _ = resp.send(Err(e.to_string()));
+                        continue;
+                    }
+                };
+                _ = resp.send(Ok(headers));
+                csv_path = Some(path);
+                csv_rdr = Some(rdr);
+            }
+            Event::Generate(image_path, indices, resp) => {
+                let Some(csv_rdr) = &mut csv_rdr else {
+                    eprintln!("no csv reader");
+                    continue;
+                };
+
+                let mut img = image::open(&image_path)?.to_rgba8();
+                let scale = PxScale::from(92.0);
+                let color = Rgba([255, 255, 0, 255]);
+
+                let headers = csv_rdr.headers()?.clone();
+                let first_row = csv_rdr.records().next().unwrap()?;
+
+                let font = &handle.state::<AppState>().font;
+
+                for (i, &col) in indices.iter().enumerate() {
+                    let offset_y = (i as i32 + 1) * 92;
+                    let k = headers[col].trim();
+                    let v = first_row[col].trim();
+                    let text = format!("{k}: {v}");
+                    draw_text_mut(&mut img, color, 92, offset_y, scale, font, &text);
+                }
+
+                fs::create_dir_all(&outputs_dir).unwrap();
+                img.save(&output_file)?;
+
+                println!("generated {output_file:?}");
+
+                _ = resp.send(Ok(output_file.clone()));
+            }
+            Event::Save(dest, resp) => {
+                match fs::copy(&output_file, &dest) {
+                    Ok(_) => {
+                        println!("saved {dest:?}");
+                        let _ = resp.send(Ok(dest));
+                    }
+                    Err(e) => _ = resp.send(Err(e.to_string())),
+                };
+            }
+        }
+    }
+    Ok(())
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn load_csv(path: PathBuf, state: State<'_, AppState>) -> Result<Vec<String>, String> {
+    let (tx, rx) = oneshot::channel();
+    state.tx.send(Event::Csv(path, tx)).await.unwrap();
+    rx.await.unwrap()
+}
+
+#[tauri::command]
+async fn generate(
+    image_path: PathBuf,
+    indices: Vec<usize>,
+    state: State<'_, AppState>,
+) -> Result<PathBuf, String> {
+    let (tx, rx) = oneshot::channel();
+    state
+        .tx
+        .send(Event::Generate(image_path, indices, tx))
+        .await
+        .unwrap();
+    rx.await.unwrap()
+}
+
+#[tauri::command]
+async fn save(dest: PathBuf, state: State<'_, AppState>) -> Result<PathBuf, String> {
+    let (tx, rx) = oneshot::channel();
+    state.tx.send(Event::Save(dest, tx)).await.unwrap();
+    rx.await.unwrap()
+}
+
+struct AppState {
+    font: FontArc,
+    tx: mpsc::Sender<Event>,
+}
+
+fn setup(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    const FONT_DATA: &[u8] = include_bytes!("../../public/arial.ttf");
+
+    let (tx, rx) = mpsc::channel(100);
+    {
+        let handle = app.handle().clone();
+        thread::spawn(move || {
+            if let Err(e) = worker(handle, rx) {
+                eprintln!("worker failed; cause = {e}");
+            }
+        });
+    }
+
+    let state = AppState {
+        font: FontArc::try_from_slice(FONT_DATA)?,
+        tx,
+    };
+
+    app.manage(state);
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![load_csv, generate, save])
+        .setup(setup)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,49 +1,231 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import { invoke } from "@tauri-apps/api/core"
-import "./styles.css"
+import { useRef, useState } from "react"
+
+import * as dialog from "@tauri-apps/plugin-dialog"
+import { convertFileSrc, invoke, InvokeArgs } from "@tauri-apps/api/core"
+import { basename } from "@tauri-apps/api/path"
+
+import { Input } from "./components/ui/input"
+import { Field, FieldGroup, FieldLabel } from "./components/ui/field"
+import { Card, CardContent, CardFooter } from "./components/ui/card"
+import { Button } from "./components/ui/button"
+import { ButtonGroup } from "./components/ui/button-group"
+import { Checkbox } from "./components/ui/checkbox"
+import { Label } from "./components/ui/label"
+import { Spinner } from "./components/ui/spinner"
+import { CircleAlertIcon, SaveIcon } from "lucide-react"
+
+export function TailwindIndicator() {
+  return (
+    <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">sm</div>
+      <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">md</div>
+      <div className="hidden lg:block xl:hidden 2xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  )
+}
+
+function FileInput(props: dialog.OpenDialogOptions & { onChange?: (path?: string) => void }) {
+  const ref = useRef<HTMLInputElement>(null)
+  return (
+    <ButtonGroup>
+      <Button
+        variant="secondary"
+        onClick={async () => {
+          const path = await dialog.open(props)
+          ref.current!.value = path ? await basename(path) : ""
+          props.onChange?.(path || undefined)
+        }}
+      >
+        Choose file
+      </Button>
+      <Input ref={ref} disabled={true} />
+    </ButtonGroup>
+  )
+}
+
+function useIPC<T>(
+  name: string,
+  opts?: {
+    onSuccess?: (data: T) => void
+    onError?: (err: string) => void
+  },
+) {
+  const [data, setData] = useState<T>()
+  const [error, setError] = useState<string>()
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function send(args: InvokeArgs) {
+    setError(undefined)
+    setIsLoading(true)
+    try {
+      const res = await invoke<T>(name, args)
+      setData(res)
+      opts?.onSuccess?.(res)
+    } catch (err: any) {
+      setError(err)
+      setData(undefined)
+      opts?.onError?.(err)
+    }
+    setIsLoading(false)
+  }
+
+  return {
+    send,
+    data,
+    error,
+    isLoading,
+  }
+}
 
 export function App() {
-  const [greetMsg, setGreetMsg] = useState("")
-  const [name, setName] = useState("")
+  const [csvPath, setCsvPath] = useState<string>()
+  const [imagePath, setImagePath] = useState<string>()
+  const [wantHeaders, setWantHeaders] = useState<number[]>([])
+  const [imageSrc, setImageSrc] = useState<string>()
+  const [error, setError] = useState<string>()
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }))
+  const headers = useIPC<string[]>("load_csv", {
+    onSuccess() {
+      setWantHeaders([])
+    },
+    onError(err) {
+      setError(err)
+    },
+  })
+
+  const generate = useIPC<string>("generate", {
+    onSuccess(path) {
+      setImageSrc(convertFileSrc(path))
+    },
+    onError(err) {
+      setError(err)
+    },
+  })
+
+  const saveOnDisk = useIPC<string>("save", {
+    onSuccess(dest) {
+      console.log(`saved at ${dest}`)
+    },
+    onError(err) {
+      setError(err)
+    },
+  })
+
+  async function onSubmit() {
+    if (!imagePath && !csvPath) {
+      setError("Provide both an image and a CSV.")
+      return
+    }
+
+    if (wantHeaders.length === 0) {
+      setError("Must select headers")
+      return
+    }
+
+    const indices = wantHeaders.sort((a, b) => a - b)
+    await generate.send({ imagePath, indices })
   }
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <main className="relative h-screen flex flex-col items-center">
+      <div className="container grid grid-cols-1 lg:grid-cols-2 pt-8 gap-4">
+        <Card className="flex flex-col h-full">
+          <CardContent className="flex flex-col gap-y-8">
+            <FieldGroup className="grid grid-cols-2">
+              <Field>
+                <FieldLabel>Log file</FieldLabel>
+                <FileInput
+                  onChange={async (path) => {
+                    if (path !== csvPath) {
+                      setCsvPath(path)
+                      await headers.send({ path })
+                    }
+                  }}
+                />
+              </Field>
+              <Field>
+                <FieldLabel>Image</FieldLabel>
+                <FileInput
+                  filters={[
+                    {
+                      name: "images",
+                      extensions: ["jpg", "jpeg", "png", "webp"],
+                    },
+                  ]}
+                  onChange={(path) => {
+                    if (path !== imagePath) {
+                      setImagePath(path)
+                    }
+                  }}
+                />
+              </Field>
+            </FieldGroup>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+            {headers.data && (
+              <FieldGroup>
+                <Field>
+                  <FieldLabel>Headers</FieldLabel>
+                  <FieldGroup className="grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] max-h-52 overflow-auto">
+                    {headers.data.map((header, i) => (
+                      <Field key={header} orientation="horizontal">
+                        <Checkbox
+                          onCheckedChange={(v) => {
+                            if (v) {
+                              setWantHeaders((prev) => [...prev, i])
+                            } else {
+                              setWantHeaders((prev) => prev.filter((n) => n !== i))
+                            }
+                          }}
+                        />
+                        <Label>{header}</Label>
+                      </Field>
+                    ))}
+                  </FieldGroup>
+                </Field>
+              </FieldGroup>
+            )}
+            {error && (
+              <div className="text-destructive flex items-center gap-x-2">
+                <CircleAlertIcon />
+                <span>{error}</span>
+              </div>
+            )}
+          </CardContent>
+          <CardFooter className="justify-between">
+            <Button onClick={onSubmit} disabled={generate.isLoading}>
+              {generate.isLoading && <Spinner />}
+              Generate
+            </Button>
+            <Button
+              disabled={!imageSrc}
+              onClick={async () => {
+                const dest = await dialog.save({
+                  canCreateDirectories: true,
+                  filters: [{ name: "filter", extensions: ["png"] }],
+                })
+                if (!dest) return
+                await saveOnDisk.send({ dest })
+              }}
+            >
+              <SaveIcon />
+              Save
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="relative py-0">
+          {imageSrc ? (
+            <img className="object-cover w-full h-full" src={imageSrc} />
+          ) : (
+            <div className="flex w-full h-full items-center justify-center text-muted-foreground self-auto">
+              Image preview
+            </div>
+          )}
+        </Card>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+      <TailwindIndicator />
     </main>
   )
 }
